05bed1ac4bb533af3c14c6fec5905a8c0f06056a
Bratini89 <carlosbratini@hotmail.com>
2023-01-19 01:41:20 -0400

    Arreglando proyecto practica

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,29 +1,38 @@
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
-
 const authMiddleware = (req, res, next) => {
   let { authorization: token } = req.headers;
-  token = token.replace("Bearer", "");
+  token = token?.replace("Bearer ", "");
   console.log(token);
-  jwt.verify(
-    token,
-    process.env.JWT_SECRET,
-    { algorithms: "HS512" },
-    (err, decoded) => {
-      if (err) {
-        res.status(400).json({
-          error: "invalid token",
-          message: "El token no es valido, enviar el token correcto.",
-        });
-      } else {
-        console.log(decoded);
-        next();
+  if (token) {
+    jwt.verify(
+      token,
+      process.env.JWT_SECRET,
+      { algorithms: "HS512" },
+      (err, decoded) => {
+        if (err) {
+          res
+            .status(400)
+            .json({
+              error: "invalid token",
+              message:
+                "El token no es válido o ya expiro, envía un token correcto",
+            });
+        } else {
+          console.log(decoded);
+          next();
+        }
       }
-    }
-  );
-  console.log(decoded);
+    );
+  } else {
+    res
+      .status(400)
+      .json({
+        error: "no token provided",
+        message: "No se esta enviando un token de autenticación",
+      });
+  }
 };
-
 module.exports = authMiddleware;
 
 // vamos a validar el token
